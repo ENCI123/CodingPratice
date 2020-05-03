8b352cc27c95a5e5cb72dc619b84071cb90c730a
Enci Lin <lin.tianci123@gmail.com>
2020-05-03 15:20:39 -0400

    Update LongestCommonPrefixEL.py
    Fix the bugs where skip the last index of str list for comparison.

--- LongestCommonPrefixEL.py
+++ LongestCommonPrefixEL.py
@@ -2,23 +2,26 @@ class Solution:
     "Took longer time to finish"
     "Not Complete"
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        CommonPrefix = ""
-        reference = strs[0]
-        result = ""
-        for i in range(len(strs)-1):
-        for i in range(1,len(strs)-1):
-            CommonPrefix  = ""
+        if(len(strs)==0):
+            return ""
+        
+        reference = strs[0] 
+        for i in range(1,len(strs)):
+            CommonPrefix = ""
             if(len(reference) <= len(strs[i])):
                 ind = len(reference)
             else:
                 ind = len(strs[i])
+            
             for j in range(ind):
                 if reference[j] == strs[i][j]:
-                    CommonPrefix =  CommonPrefix+reference[j]
-                    
-                elif reference[j] != sts[i][j]:
+                    CommonPrefix = CommonPrefix + reference[j]
+                   
+                else:
                     break
+                
             reference = CommonPrefix
             
+           
         return reference
         
